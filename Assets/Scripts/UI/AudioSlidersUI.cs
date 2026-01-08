4ae535b2157253ee4e6b7b823d8bee29a8adfabc
Alexios <ashiacolas2806@gmail.com>
2026-01-08 13:03:54 +0200

    updated audio

--- Assets/Scripts/UI/AudioSlidersUI.cs
+++ Assets/Scripts/UI/AudioSlidersUI.cs
@@ -6,13 +6,13 @@ public class AudioSlidersUI : MonoBehaviour
     [SerializeField] private Slider masterSlider;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
-    [SerializeField] private Slider voiceSlider;
+    //[SerializeField] private Slider voiceSlider;
 
     [Header("Default slider values (0..1)")]
     [Range(0f, 1f)][SerializeField] private float masterDefault = 1f;
     [Range(0f, 1f)][SerializeField] private float musicDefault  = 1f;
     [Range(0f, 1f)][SerializeField] private float sfxDefault    = 1f;
-    [Range(0f, 1f)][SerializeField] private float voiceDefault  = 1f;
+    //[Range(0f, 1f)][SerializeField] private float voiceDefault  = 1f;
 
     private void Start()
     {
@@ -27,18 +27,18 @@ public class AudioSlidersUI : MonoBehaviour
         if (masterSlider) masterSlider.SetValueWithoutNotify(masterDefault);
         if (musicSlider)  musicSlider.SetValueWithoutNotify(musicDefault);
         if (sfxSlider)    sfxSlider.SetValueWithoutNotify(sfxDefault);
-        if (voiceSlider)  voiceSlider.SetValueWithoutNotify(voiceDefault);
+        //if (voiceSlider)  voiceSlider.SetValueWithoutNotify(voiceDefault);
 
         // Apply default volumes
         am.SetMasterVolume01(masterDefault);
         am.SetMusicVolume01(musicDefault);
         am.SetSfxVolume01(sfxDefault);
-        am.SetVoiceVolume01(voiceDefault);
+        am.SetVoiceVolume01(sfxDefault);
 
         // Add listeners to sliders
         if (masterSlider) masterSlider.onValueChanged.AddListener(am.SetMasterVolume01);
         if (musicSlider)  musicSlider.onValueChanged.AddListener(am.SetMusicVolume01);
         if (sfxSlider)    sfxSlider.onValueChanged.AddListener(am.SetSfxVolume01);
-        if (voiceSlider)  voiceSlider.onValueChanged.AddListener(am.SetVoiceVolume01);
+        if (sfxSlider)  sfxSlider.onValueChanged.AddListener(am.SetVoiceVolume01);
     }
 }
